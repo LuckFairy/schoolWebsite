c0004a0db74191d38cb94ce18c2e00f09bac2443
chentian <326932483@qq.com>
2018-10-19 15:52:17 +0800

    修改scss配置，覆盖element-ui自定义主题

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/element-variables.scss'
 import App from './App'
 import router from './router'
 import common from './api/common'
